1bfd49a0b55b9686438c750f4d337651bd95f598
Evan Longwood <evanmlongwood@gmail.com>
2018-01-24 16:42:48 -0800

    Just a Javascript example.

---
+++ Nanodegree_Front_End_Web_Development/Javascript/Flowchart Example/Flowchart.js
@@ -0,0 +1,18 @@
+var price = 15.00; // price of our hammer
+var money = 20.00; //how much money I have
+
+if (money >= price) {
+    console.log("Buy the hammer");
+} else {
+    console.log("Don't buy the hammer");
+}
+
+//----------Reverse Condition-------------
+// var price = 15.00; // price of our hammer
+// var money = 10.00; //how much money I have
+
+// if (money >= price) {
+//     console.log("Buy the hammer");
+// } else {
+//     console.log("Don't buy the hammer");
+// }
