eea87e86ff603a6d3903e082e049c45e2b2e4690
Evan Longwood <evanmlongwood@gmail.com>
2018-02-01 12:43:24 -0800

    Quiz addition.

---
+++ Nanodegree_Front_End_Web_Development/Javascript/Flowchart Example/laughitoffagain.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Laugh (5-4)
+ */
+
+var laugh = /* finish the function expression */ function(laugh){
+    var ha = "";
+    for (var x = 0; x < laugh; x++){
+        ha += "ha";
+    }
+   return ha + "!";
+};
+console.log(laugh(10));
