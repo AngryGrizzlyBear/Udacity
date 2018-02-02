6b9ae87d34f0ac0b3ea45b116cc843ef1481557b
Evan <evanmlongwood@gmail.com>
2018-02-01 16:10:47 -0800

    Need to review this gain in the future.

---
+++ Nanodegree_Front_End_Web_Development/Javascript/Flowchart Example/HarryPotter.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Quidditch Cup (6-5)
+ */
+
+// your code goes here
+function hasEnoughPlayers(yee){
+    return yee.length >=7;
+}
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+console.log(hasEnoughPlayers(team));
+
+
+//Gunna have to review this one again a little later
